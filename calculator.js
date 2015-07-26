dcee01dc333f291f288311272d0ae14aa724604a
mdctenga <mdctenga@gmail.com>
2015-07-25 23:40:47 -1000

    Implement tests

---
+++ calculator.js
@@ -0,0 +1,5 @@
+module.exports = calculator;
+
+function calculator () {
+
+}
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+var calculator = require('../calculator.js');
+
+describe('', function(){
+  it('should be a function', function() {
+    expect(calculator).to.be.a('function');
+  });
+});
